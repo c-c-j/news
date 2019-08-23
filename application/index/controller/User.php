fddce247e671d0051ffaec0828edcbedb6781cb0
jizi <15820363688@163.com>
2019-08-24 07:37:26 +0800

    增加用户注册、登录、退出功能

---
+++ application/index/controller/User.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace app\index\controller;
+
+use app\index\model\User as UserModel;
+use think\Controller;
+use think\Request;
+use think\Session;
+
+/**
+ * 前台用户控制器
+ */
+class User extends Controller
+{
+	/**
+	 * 用户个人中心首页
+	 * 
+	 */
+	public function index()
+	{
+		return $this->fetch();
+	}
+
+	/**
+	 * 用户登录页面
+	 * 
+	 */
+	public function login()
+	{
+		return $this->fetch();
+	}
+
+	/**
+	 * 用户登录表单提交
+	 * 
+	 */
+	public function doLogin()
+	{
+		$data = input('param.');
+
+		$user = db('user')
+		->where('username','eq',$data['username'])
+		->where('password','eq',md5($data['password']))
+		->find();
+
+		if ( $user) {
+			Session::set('user_name',$user);
+			Session::set('user_id',$id);
+			$this->success('登录成功');
+			exit();
+		}
+
+		$this->error('登录失败');
+
+	}
+
+	/**
+	 * 用户退出
+	 * 
+	 */
+	public function logout()
+	{
+		Session::delete('user_name');
+		Session::delete('user_id');
+
+		$this->redirect(url('index/index/index'));
+	}
+
+	/**
+	 * 用户注册页面
+	 * 
+	 */
+	public function register()
+	{
+		return $this->fetch();
+	}
+
+	/**
+	 * 执行用户表单提交
+	 * 
+	 */
+	public function doRegister(Request $request)
+	{
+		$data = $request->post();
+        if ($data['password'] != $data['repassword']) {
+            $this->error("两次输入的密码不一致");
+        }
+
+        $data['password'] = md5($data['password']);
+
+        $res = model('user')
+        ->allowField(true) // 过滤非数据表字段(repassword)
+            ->save($data);
+        if ($res) {
+            $this->success("注册成功");
+        } else {
+            $this->error("注册失败");
+        }
+	}
+}
