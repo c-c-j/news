36be549cb79b24180283aa903126356e8ef688d5
jizi <15820363688@163.com>
2019-08-24 21:55:25 +0800

    修改右侧榜单变量的初始化

--- application/index/controller/Blog.php
+++ application/index/controller/Blog.php
@@ -11,6 +11,29 @@ use think\Controller;
  */
 class  Blog extends Controller
 {
+
+    /**
+     * 初始化方法
+     */
+    public function _initialize()
+    {
+        // 查看最新的5条记录
+        $newest = db('blog')
+            ->field("id, title")
+            ->order('created DESC')
+            ->limit(5)
+            ->select();
+        // 查看最热的5条记录
+        $hotest = db('blog')
+            ->field("id, title, view")
+            ->order('view DESC')
+            ->limit(5)
+            ->select();
+
+        $this->assign('newest', $newest);
+        $this->assign('hotest', $hotest);
+    }
+
 	public function index()
 	{
 		//分页查询博客列表
@@ -18,24 +41,8 @@ class  Blog extends Controller
 		->order('created DESC')
 		->paginate(5);
 
-		//查看最新的5条博客记录
-		$newest = db('blog')
-			->field('id, title')
-			->order('created DESC')
-			->limit(5)
-			->select();
-		
-		//查看最热的五条博客记录
-		$hotest = db('blog')
-			->field('id, title, view')
-			->order('view DESC')
-			->limit(5)
-			->select();
-
 		$this->assign([
 			'list' => $list,
-			'newest' => $newest,
-			'hotest' => $hotest
 		]);
 
 		return $this->fetch();
@@ -51,25 +58,11 @@ class  Blog extends Controller
         # 接收id参数
         $id = input('param.id');
 
-        $data = BlogModel::get($id); 
-
-        // 查看最新的5条记录
-        $newest = db('blog')
-            ->field("id, title")
-            ->order('created DESC')
-            ->limit(5)
-            ->select();
-
-        // 查看最热的5条记录
-        $hotest = db('blog')
-            ->field("id, title, view")
-            ->order('view DESC')
-            ->limit(5)
-            ->select();
-
         // 更新博客的浏览量
         db('blog')->where('id', 'eq', $id)
             ->setInc('view');
+        
+        $data = BlogModel::get($id); 
 
         // 将查到的结果赋值给模板
         $this->assign('data', $data);
@@ -100,7 +93,7 @@ class  Blog extends Controller
     public function doAdd()
     {
         $data = input('param.');
-        
+
         $data['uid'] = session('user.id');
 
         // 处理图片的上传
