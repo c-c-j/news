630601dafeb29c9d00103edcc63902a7ba8c9e14
jizi <15820363688@163.com>
2019-08-24 20:00:45 +0800

    修复添加博客bug

--- application/index/controller/Blog.php
+++ application/index/controller/Blog.php
@@ -79,4 +79,53 @@ class  Blog extends Controller
         return $this->fetch();
     }
 
+    /**
+     * 博客添加页面
+     */
+    public function add()
+    {
+        // 判断用户有没有登陆
+        if (session('?user')) {
+            // 调用模板
+            return $this->fetch();
+        } else {
+            // 如果没有登陆,则跳转到登陆页面
+            $this->error("请先登陆...", url('index/user/login'));
+        }
+    }
+
+        /**
+     * 添加博客的提交方法
+     */
+    public function doAdd()
+    {
+        $data = input('param.');
+        
+        $data['uid'] = session('user.id');
+
+        // 处理图片的上传
+        // 1. 接收数据
+        $file = request()->file('image');
+        if ($file) {
+            // 2. 执行上传
+            $path = ROOT_PATH . 'public/static/upload';
+            $info = $file
+                ->validate(['size' => 2048000, 'ext' => 'jpg,png,gif'])
+                ->move($path);
+            if (is_object($info) && !empty($info->getSaveName())) {
+                // 将图片的上传路径保存到数据库
+                $data['image'] = $info->getSaveName();
+            } else {
+                $this->error($file->getError());
+            }
+        }
+
+        // 使用模型方式添加数据
+        $res = model('blog')->save($data);
+        if ($res) {
+            $this->success("添加成功");
+        } else {
+            $this->error("添加失败");
+        }
+    }
 }
