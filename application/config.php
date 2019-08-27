abfc59a077649acfac439e894f7ba66fb6698944
jizi <15820363688@163.com>
2019-08-27 15:06:26 +0800

    修改替换错误

--- application/config.php
+++ application/config.php
@@ -144,8 +144,8 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        " " => "&nbsp;",
-        "r" => "<br/>"
+        "" => "&nbsp;",
+        "\r" => "<br/>"
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
