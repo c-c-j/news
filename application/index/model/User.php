e854743bc92d120a94fff8ae23bced7899f3a226
jizi <15820363688@163.com>
2019-08-27 14:14:04 +0800

    增加首页最新博客的五条记录图片轮播图效果 用户注册通过修改器自动加密

--- application/index/model/User.php
+++ application/index/model/User.php
@@ -14,6 +14,19 @@ class User extends Model
     protected $updateTime         = false;
     protected $createTime         = "created";
 
+    // 添加场景下,需要自动完成的字段
+    protected $insert = [
+        'balance' => 100,
+    ];
+
+    /**
+     * 通过修改器,对密码进行md5加密
+     */
+    public function setPasswordAttr($value)
+    {
+        return md5($value);
+    }
+
     /**
      * 根据一对多关系,来获取某个用户下的博客
      *
@@ -21,7 +34,7 @@ class User extends Model
      */
     public function blogs()
     {
-        //                   关联模型
+        //关联模型
         return $this->hasMany('Blog', 'uid')
             ->order('created DESC')
             ->paginate(5);
