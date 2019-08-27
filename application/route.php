b41573fa10a5cd41b1d786456822ae64b8a3d7c6
jizi <15820363688@163.com>
2019-08-27 18:03:58 +0800

    设置路由配置

--- application/route.php
+++ application/route.php
@@ -8,6 +8,13 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
+use think\Route;
+
+// 设置首页的访问路由
+
+Route::rule('/', 'index/index/index', 'get');
+Route::get('user/login', 'index/user/login');
+Route::post('user/doLogin', 'index/user/doLogin');
 
 return [
     '__pattern__' => [
@@ -16,6 +23,27 @@ return [
     '[hello]'     => [
         ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
         ':name' => ['index/hello', ['method' => 'post']],
+    ],
+
+    // 方式二: 路由分组
+    '[blog]'      => [
+        ':id'   => [
+            'index/blog/detail',
+            ['method' => 'get'],
+            ['id' => '\d+'],
+        ],
+        'add'   => [
+            'index/blog/add',
+            ['method' => 'get'],
+        ],
+        'doAdd' => [
+            'index/blog/doAdd',
+            ['method' => 'post'],
+        ],
+        '/'     => [
+            'index/blog/index',
+            ['method' => 'get'],
+        ],
     ],
 
 ];
